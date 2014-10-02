208765408eb3bd8da6bef126a41551b53027d74c
Jeffrey Payne <jeff@flexrentalsolutions.com>
2014-10-02 10:26:04 -0700

    Tweaking map function.

--- src/main/resources/com/frs/alto/dao/couchbase/map-template.js
+++ src/main/resources/com/frs/alto/dao/couchbase/map-template.js
@@ -2,12 +2,12 @@ function(doc, meta) {
 	var keyTokens = meta.id.split("#");
 	if (keyTokens.length == 3) {
 		if (keyTokens[1] == '$keyNameSpace') {
-			emit([keyTokens[0], keyTokens[2]], meta.id);
+			emit([keyTokens[0], keyTokens[2]], keyTokens[2]);
 		}
 	}
 	else {
 		if (keyTokens[0] == '$keyNameSpace') {
-			emit(keyTokens[1], meta.id);
+			emit(keyTokens[1], keyTokens[1]);
 		}
 	}
 }
